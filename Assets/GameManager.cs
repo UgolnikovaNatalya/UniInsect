3602b6b77becc67edad3456ac2e02ab189a02384
Ugolnikova Natalia <ugolok777@gmail.com>
2023-06-07 19:27:02 +0300

    Изменение ошибок

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private string count;
+    public Text finalScore;
+
+    private void Start()
+    {
+        count = "0";
+        finalScore.text = "0";
+    }
+
     public void EndGame()
     {
-        Debug.Log("GameOver");
+
+        count = finalScore.text;
+
+        Debug.LogError($"Finale score is: {count}");
+
     }
 
 }
